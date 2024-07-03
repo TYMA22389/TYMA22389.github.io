789318af45296260cfed53f05876efdf4a5a677f
TYMA22389 <TYMA22389@yandex.ru>
2024-07-03 17:22:39 +0300

    Secret
    Shhhh!

---
+++ captcha1_ajax.php
@@ -0,0 +1,11 @@
+<?php
+session_start();
+if (isset($_POST['value'],$_SESSION['captcha']) && md5($_POST['value']) == $_SESSION['captcha'])
+{
+   echo "true";
+}
+else
+{
+   echo "false";
+}
+?>
